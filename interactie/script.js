6a796c7b4591e4a8297a723d24539cef4bd05e26
Delamox <129861741+Delamox@users.noreply.github.com>
2023-10-25 10:25:12 +0200

    les af

--- interactie/script.js
+++ interactie/script.js
@@ -1,19 +1,38 @@
-function clrBall()
+function init()
     {
         document.getElementById('ball').style.backgroundColor = "#FF88FF";
-        document.getElementById('ball').style.width = "200px";
-        document.getElementById('ball').style.height = "200px";
+        document.getElementById('ball').style.width = "400px";
+        document.getElementById('ball').style.height = "400px";
+        document.getElementById('ball').style.position = "relative";
+        document.getElementById('text').style.position = "absolute";
+        document.getElementById('text').style.color = "#FF88FF"
+        document.getElementById('text').style.top = "50%";
+        document.getElementById('text').style.left = "50%";
+        document.getElementById('text').style.transform = "translate(-50%, -50%)";
     }
-clrBall();
+init();
 
 function msEnter()
     {
+        var max = 85;
+        var min = 15;
+        var randXPosInt = Math.random() * (max - min) + min;
+        var randYPosInt = Math.random() * (max - min) + min;
         document.getElementById('ball').style.backgroundColor = "#FF8800";
+        document.getElementById('text').style.top = randYPosInt + "%";
+        document.getElementById('text').style.left = randXPosInt + "%";
     }
 
 function msLeave()
     {
         document.getElementById('ball').style.backgroundColor = "#FF88FF";
+        document.getElementById('text').innerHTML = "missed!"
     }
-document.getElementById('ball').onmouseenter = function() {msEnter()};
-document.getElementById('ball').onmouseleave = function() {msLeave()};
+
+function msClick()
+    {
+        document.getElementById('text').innerHTML = "got me!"
+    }
+document.getElementById('ball').onmousedown = function() {msEnter()};
+document.getElementById('ball').onmouseup = function() {msLeave()};
+document.getElementById('text').onclick = function() {msClick()};
