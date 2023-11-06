85b4e3de0edf9616189ade809b1ea3c76a3caa55
Delamox <129861741+Delamox@users.noreply.github.com>
2023-11-06 14:14:20 +0100

    short and fix

--- interactie/script.js
+++ interactie/script.js
@@ -1,16 +1,18 @@
 function init()
     {
-        document.getElementById('ball').style.backgroundColor = "#FF88FF";
-        document.getElementById('ball').style.width = "400px";
-        document.getElementById('ball').style.height = "400px";
-        document.getElementById('ball').style.position = "relative";
-        document.getElementById('text').style.position = "absolute";
-        document.getElementById('text').style.color = "#FF88FF"
-        document.getElementById('text').style.top = "50%";
-        document.getElementById('text').style.left = "50%";
-        document.getElementById('text').style.transform = "translate(-50%, -50%)";
-    }
-init();
+        var ball = document.getElementById('ball');
+        var text = document.getElementById('text');
+        ball.style.backgroundColor = "#FF88FF";
+        ball.style.width = "400px";
+        ball.style.height = "400px";
+        ball.style.position = "relative";
+        text.style.position = "absolute";
+        text.style.color = "#FF88FF"
+        text.style.backgroundColor = "#FF88FF"
+        text.style.top = "50%";
+        text.style.left = "50%";
+        text.style.transform = "translate(-50%, -50%)";
+    } init();
 
 function msEnter()
     {
@@ -18,21 +20,23 @@ function msEnter()
         var min = 15;
         var randXPosInt = Math.random() * (max - min) + min;
         var randYPosInt = Math.random() * (max - min) + min;
-        document.getElementById('ball').style.backgroundColor = "#FF8800";
-        document.getElementById('text').style.top = randYPosInt + "%";
-        document.getElementById('text').style.left = randXPosInt + "%";
+        ball.style.backgroundColor = "#FF8800";
+        text.style.backgroundColor = "#FF8800";
+        text.style.top = randYPosInt + "%";
+        text.style.left = randXPosInt + "%";
     }
 
 function msLeave()
     {
-        document.getElementById('ball').style.backgroundColor = "#FF88FF";
-        document.getElementById('text').innerHTML = "missed!"
+        ball.style.backgroundColor = "#FF88FF";
+        text.innerHTML = "missed!";
+        text.style.backgroundColor = "#FF88FF";
     }
 
 function msClick()
     {
-        document.getElementById('text').innerHTML = "got me!"
+        text.innerHTML = "got me!";
     }
-document.getElementById('ball').onmousedown = function() {msEnter()};
-document.getElementById('ball').onmouseup = function() {msLeave()};
-document.getElementById('text').onclick = function() {msClick()};
+ball.onmousedown = function() {msEnter()};
+ball.onmouseup = function() {msLeave()};
+text.onclick = function() {msClick()};
