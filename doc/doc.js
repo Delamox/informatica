ce1c406e7d617d3bd004bdae1f0d8f7b542f6aca
Delamox <129861741+Delamox@users.noreply.github.com>
2023-11-13 14:18:43 +0100

    new

---
+++ doc/doc.js
@@ -0,0 +1,22 @@
+//ecma
+(()=>
+{
+
+})(window,document)
+
+//js
+document.addEventListener("DOMContentLoaded", function()
+{
+
+})
+
+//js2
+(function()
+{
+
+})(window,document)
+
+//object
+var x = 1
+const y = x
+let z = x
