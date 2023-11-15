3bf15cafa5beab43c0040c63725a78987dd670e2
Delamox <129861741+Delamox@users.noreply.github.com>
2023-11-15 17:47:47 +0100

    beep boop

--- calc/script.js
+++ calc/script.js
@@ -1,7 +1,6 @@
 var disp=[,];
 var i=0
 var op=''
-var fv=document.getElementById('dp').value
 disp[0]=''
 disp[1]='' 
 //i dicteert het nummer na [1] of voor [0] de operator in disp[]
@@ -12,29 +11,30 @@ function addToDisplay(e)
 {
     if(ans=!true){disp[i]+=e}else{disp[i]=e}
     ans=false
-    fv=disp[i]
+    document.getElementById('dp')=disp[i]
 }
 function switchInt(e)
 {
     op=e
     if (i==0){i=1}
     else if (i==1){i=0}
-    fv=disp[i]
+    document.getElementById('dp')=disp[i]
 }
 function backspace()
 {
     disp[i]=disp[i].slice(0,-1)
-    fv=(disp[i])
+    document.getElementById('dp')=(disp[i])
 }
 function dispclear()
 {
     i=0
     op=''
     disp=['','']
-    fv=''
+    document.getElementById('dp')=''
 }
 function calculate()
 {
+    try{
     if (op=='+')
     {res=parseInt(disp[0])+parseInt(disp[1])}
     else if (op=='-')
@@ -43,9 +43,14 @@ function calculate()
     {res=disp[0]*disp[1]}
     else if (op=='/')
     {res=disp[0]/disp[1]}
-    fv=res
+    document.getElementById('dp')=res
     i=0
     disp[0]=parseInt(res)
     disp[1]='';
     ans=true
+    } catch 
+    {
+        document.getElementById('dp')='ERROR'
+        ans=true
+    }
 }
