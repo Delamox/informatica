4ab9bff8fe4b8d6a450991f266c7f955f86ebea9
Delamox <129861741+Delamox@users.noreply.github.com>
2023-11-15 15:58:58 +0100

    boop

--- calc/doc.js
+++ calc/doc.js
@@ -1,37 +1,51 @@
-var disp=[];
-var r=0
+var disp=[,];
+var i=0
 var op=''
+var fv=document.getElementById('dp').value
 disp[0]=''
 disp[1]='' 
+//i dicteert het nummer na [1] of voor [0] de operator in disp[]
+//op is de operator
+//alles zelf geschreven behalve frontend (met w3 en stackoverflow voor wat hulp bij specifieke syntax)
+
 function addToDisplay(e)
 {
-    disp[r]+=e
-    document.getElementById('dp').value=disp[r]
+    if(ans=!true){disp[i]+=e}else{disp[i]=e}
+    ans=false
+    fv=disp[i]
 }
 function switchInt(e)
 {
     op=e
-    if (r==0){r=1}
-    else if (r==1){r=0}
-    document.getElementById('dp').value=disp[r]
+    if (i==0){i=1}
+    else if (i==1){i=0}
+    fv=disp[i]
 }
-function backSpace()
+function backspace()
 {
-    disp[r]=disp[r].slice(0,-1)
-    document.getElementById('dp').value=(disp[r])
+    disp[i]=disp[i].slice(0,-1)
+    fv=(disp[i])
+}
+function dispclear()
+{
+    i=0
+    op=''
+    disp=['','']
+    fv=''
 }
 function calculate()
 {
-if (op=='+')
-{res=parseInt(disp[0])+parseInt(disp[1])}
-else if (op=='-')
-{res=disp[0]-disp[1]}
-else if (op=='*')
-{res=disp[0]*disp[1]}
-else if (op=='/')
-{res=disp[0]/disp[1]}
-document.getElementById('dp').value=res
-r=0
-disp[0]=res
-disp[1]=''
+    if (op=='+')
+    {res=parseInt(disp[0])+parseInt(disp[1])}
+    else if (op=='-')
+    {res=disp[0]-disp[1]}
+    else if (op=='*')
+    {res=disp[0]*disp[1]}
+    else if (op=='/')
+    {res=disp[0]/disp[1]}
+    fv=res
+    i=0
+    disp[0]=parseInt(res)
+    disp[1]='';
+    ans=true
 }
