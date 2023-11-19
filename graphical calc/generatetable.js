d12b453377e39daa3979c2f0b7b611c08bf0dd7f
Delamox <129861741+Delamox@users.noreply.github.com>
2023-11-19 23:44:40 +0100

    i like violins

--- graphical calc/generatetable.js
+++ graphical calc/generatetable.js
@@ -1,23 +1,36 @@
 var table = document.createElement('table');
-var Xaxis = 200;
-var Yaxis = 200;
-var fx = '80*Math.cos((2*Math.PI*x)/100)+100+.1*x'
+var Xaxis = 300;
+var Yaxis = 300;
+var fx = '80*Math.cos((2*Math.PI*x)/100)+75+.5*x'
 function resize() {
-    Xaxis = prompt('X axis length?')
-    Yaxis = prompt('Y axis length?')
-    init()
+  Xaxis = prompt('X axis length?')
+  Yaxis = prompt('Y axis length?')
+  init()
 }
 function redraw() {
   fx = prompt('formula in JS notation')
   draw()
 }
+function clearfield() {
+  cell = document.getElementsByClassName('cell')
+  for (i = 0; i < cell.length; i++) {
+    cell[i].style.background = 'white'
+  }
+}
+function pixelsize() {
+  cell = document.getElementsByClassName('cell')
+  pad = prompt('cell padding size?')
+  for (i = 0; i < cell.length; i++) {
+    cell[i].style.padding = pad+'px'
+  }
+}
 function init() {
-  try { table.replaceChildren(); } catch(err) { alert(err) }
+  try { table.replaceChildren(); } catch (err) { alert(err) }
   for (let i = 0; i < Yaxis; i++) {
     let row = table.insertRow();
     for (let j = 0; j < Xaxis; j++) {
       let cell = row.insertCell();
-      cell.setAttribute("id", 'cell')
+      cell.setAttribute("class", 'cell')
     }
   }
   document.getElementById('body').appendChild(table);
@@ -28,10 +41,10 @@ function draw() {
     try { dy = Math.round(eval(fx)) } catch { alert('formula is not correct JS'); break }
     try { table.rows[Yaxis - dy + 0].cells[x - 1].style.background = 'black' } catch { ; }
     for (let m = 0; m < (Yaxis - dy + 1) - prevY - 1; m++) {
-      try { table.rows[m + prevY + 0].cells[x - 1].style.background = 'black' } catch { ; }
+      try { table.rows[m + prevY + 0].cells[x - 1].style.background = 'dimgray' } catch { ; }
     }
     for (let m = 0; m < -1 * ((Yaxis - dy + 1) - prevY) - 1; m++) {
-      try { table.rows[prevY - m - 2].cells[x - 2].style.background = 'black' } catch { ; }
+      try { table.rows[prevY - m - 2].cells[x - 2].style.background = 'dimgray' } catch { ; }
     }
     prevY = Yaxis - dy + 1
   }
