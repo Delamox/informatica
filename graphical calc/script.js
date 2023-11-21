d5b6ca159a810ffe4ae3322dc0c3885c7af462c4
Delamox <129861741+Delamox@users.noreply.github.com>
2023-11-21 01:24:18 +0100

    multi input

--- graphical calc/script.js
+++ graphical calc/script.js
@@ -2,14 +2,24 @@ var table=document.createElement('table')
 var Xaxis=300
 var Yaxis=300
 var fx='80*Math.cos((2*Math.PI*x)/100)+75+.5*x'
+var clra=['#ff0000','#00ff00','#0000ff','#ffff00','#ff00ff','#00ffff']
+var ci=0
+//var fxa=['','','','','','']
+//&&fxa[i]!=fx
+
 function resize(){
   Xaxis=prompt('X axis length?')
   Yaxis=prompt('Y axis length?')
   init()
 }
 function redraw(){
-  fx=prompt('formula in JS notation')
-  draw()
+  clearfield()
+  ci=0
+  for (let i=0;i<6;i++){
+    fx=document.getElementById(i).value
+      ci=i
+      draw(document.getElementById(i).value)//fxa[i]=fx
+  }
 }
 function clearfield(){
   cell=document.getElementsByClassName('cell')
@@ -21,31 +31,38 @@ function pixelsize(){
   for(i=0;i<cell.length;i++){cell[i].style.padding=pad+'px'}
 }
 function init(){
-  try{table.replaceChildren()}catch(err){alert(err)}
+  table.replaceChildren()
   for(i=0;i<Yaxis;i++){
     let row=table.insertRow()
     for (j=0;j<Xaxis;j++){
       let cell=row.insertCell()
       cell.setAttribute("class",'cell')
     }
-  }
-  document.getElementById('body').appendChild(table);
+  }document.getElementById('body').appendChild(table);
 }
 function draw(){
   prevY=0
+  clr=clra[ci%6]
   for (let x=0;x<Xaxis+1;x+=1) {
     try {dy=Math.round(eval(fx))}catch{alert('formula is not correct JS');break}
-    try {table.rows[Yaxis-dy+0].cells[x-1].style.background='black'}catch{}
+    try {table.rows[Yaxis-dy+0].cells[x-1].style.background=clr}catch{}
     if (!((dy>Yaxis||dy<0)&&(prevY>Yaxis||prevY<0))){
       for (m=0;m<(Yaxis-dy+1)-prevY-1;m++){
-        if (m<Math.floor(((Yaxis-dy+1)-prevY-1)/2)){try{table.rows[m+prevY+0].cells[x-2].style.background='dimgray'}catch{}}
-        else{try{table.rows[m+prevY+0].cells[x-1].style.background='dimgray'}catch{}} }
+        if (m<Math.floor(((Yaxis-dy+1)-prevY-1)/2)){
+          try{table.rows[m+prevY+0].cells[x-2].style.background=clr}catch{}
+        }else{
+          try{table.rows[m+prevY+0].cells[x-1].style.background=clr}catch{}
+        }
+      }
       for (m=0;m<-1*((Yaxis-dy+1)-prevY)-1;m++){
-        if (m<Math.ceil((-1*((Yaxis-dy+1)-prevY)-1)/2)){try{table.rows[prevY-m-2].cells[x-2].style.background='dimgray'}catch{}}
-        else{try{table.rows[prevY-m-2].cells[x-1].style.background='dimgray'}catch{}}}
-      prevY=Yaxis-dy+1
+        if (m<Math.ceil((-1*((Yaxis-dy+1)-prevY)-1)/2)){
+          try{table.rows[prevY-m-2].cells[x-2].style.background=clr}catch{}
+        }else{
+          try{table.rows[prevY-m-2].cells[x-1].style.background=clr}catch{}
+        }
+      }prevY=Yaxis-dy+1
     }
-  }
+  }ci++
 }
 init();
 draw();
